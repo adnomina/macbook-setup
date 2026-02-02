0b7d38feb50eeb3de3b8abb31c50753d73bf8e40
Nicolas Herschke <nicolas.herschke@gmx.de>
2026-02-02 19:30:33 +0100

    feat: install mise and starship with brew

--- setup.sh
+++ setup.sh
@@ -26,15 +26,5 @@ fi
 echo "Symlinking dotfiles ..."
 stow --target="$HOME" --dir=./dotfiles fish ghostty nvim zed
 
-# Install mise for Node.js and npm management
-if ! command -v mise &> /dev/null; then
-    echo "Installing mise ..."
-
-    curl https://mise.run | sh
-    mise activate fish | source
-
-    echo "mise installed and activated."
-fi
-
 # Restart shell
 exec fish
