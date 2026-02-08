c83605292c93283b14480490c51ec1672bff8369
Nicolas Herschke <nicolas.herschke@gmx.de>
2026-02-08 17:44:16 +0100

    feat: reuse variable

--- setup.sh
+++ setup.sh
@@ -30,14 +30,16 @@ echo "Symlinking dotfiles ..."
 stow --target="$HOME" --dir=./dotfiles fish ghostty nvim zed
 
 # Add fish to list of standard shells if not already present
+echo "Configuring fish as default shell ..."
 FISH_PATH="$(which fish)"
+
 if ! grep -qx "$FISH_PATH" /etc/shells; then
   echo "Adding fish to /etc/shells ..."
   echo "$FISH_PATH" | sudo tee -a /etc/shells > /dev/null
 fi
 
 # Change shell to fish
-chsh -s $(which fish)
+chsh -s "$FISH_PATH"
 
 # Restart shell
 exec fish
