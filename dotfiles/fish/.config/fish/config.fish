68681383133dec9af50a18f28b8eec7a308d2156
Nicolas Herschke <nicolas.herschke@gmx.de>
2026-02-17 11:07:02 +0100

    feat: set default editor to Neovim

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -2,7 +2,7 @@ function fish_greeting
     printf (set_color --bold 00FFFF)"Greetings, User. Welcome to the Grid.\n"(set_color normal)
 end
 
-set -gx EDITOR zed
+set -gx EDITOR nvim
 
 alias la="ls -la"
 alias vi='nvim'
