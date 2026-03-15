2c0e86ae61e35bc4f35cf970fcb6e06d64deee46
Nicolas Herschke <nicolas.herschke@gmx.de>
2026-03-15 20:01:19 +0100

    feat: add aerospace

--- setup.sh
+++ setup.sh
@@ -27,7 +27,7 @@ fi
 
 # Symlink dotfiles with GNU Stow
 echo "Symlinking dotfiles ..."
-stow --target="$HOME" --dir=./dotfiles fish ghostty nvim zed tmux starship
+stow --target="$HOME" --dir=./dotfiles fish ghostty nvim zed tmux starship aerospace
 
 # Add fish to list of standard shells if not already present
 echo "Configuring fish as default shell ..."
